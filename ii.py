eb255a28403a0da4437e8e85cacaf0784e6af38b
89605502155 <47472417+89605502155@users.noreply.github.com>
2020-02-08 17:05:29 +0300

    sev

--- ii.py
+++ ii.py
@@ -1,6 +1,18 @@
 import numpy as np
 import pandas as pd
 import tensorly as tl
-dataset =pd.read_csv('C:/Users/admin/Desktop/курсовая 2.0/X — копия/01.txt',sep='\t')
-dataset = dataset.fillna(0)
-print(dataset)
+import os
+dataset =pd.read_csv('C:/Users/admin/Desktop/курсовая 2.0/X — копия/01.txt', sep= ' \t')
+dataset =  dataset.fillna( 0)
+fds = sorted(os.listdir('C:/Users/admin/Desktop/курсовая 2.0/X — копия/')) #формируем список файлов для считывания в цикле
+print(fds)
+k = 0
+
+while k < 35:
+    dataset = pd.read_csv('C:/Users/admin/Desktop/курсовая 2.0/X — копия/'+fds[k],sep='\t')
+    dataset = dataset.fillna(0)
+    k = k + 1
+    print(dataset)
+    print()
+    print(k)
+    print()
